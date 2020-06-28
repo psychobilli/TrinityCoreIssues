b4392164b49ae56716c77598474b2b960a38e65a
psychobilli <richardsw1123@gmail.com>
2020-06-27 22:52:32 -0400

    Updated trainer corrections

--- Scripts/TC Cata Updates/Trainer Corrections.sql
+++ Scripts/TC Cata Updates/Trainer Corrections.sql
@@ -240,3 +240,310 @@ update creature_template set gossip_menu_id = 10834 where entry = 35873;
 -- warrior
 update creature_template set gossip_menu_id = 14199 where entry = 3169;
 update creature_template set gossip_menu_id = 14199 where entry = 17480;
+
+-- Correct Profession Trainers
+
+-- add missing gossip_menu_option for Enchanters.
+insert into gossip_menu_option
+(MenuId, OptionIndex, OptionIcon, OptionText, OptionBroadcastTextId, OptionType, OptionNpcflag)
+values (4169, 0, 3, 'Train me in Enchanting.', 0, 5, 16);
+
+-- add missing gossip_menu_option for Blacksmiths.
+insert into gossip_menu_option
+(MenuId, OptionIndex, OptionIcon, OptionText, OptionBroadcastTextId, OptionType, OptionNpcflag)
+values (2741, 0, 3, 'Train me in Blacksmithing.', 0, 5, 16);
+
+-- add missing gossip_menu_option for Alchemists.
+insert into gossip_menu_option
+(MenuId, OptionIndex, OptionIcon, OptionText, OptionBroadcastTextId, OptionType, OptionNpcflag)
+values (4111, 0, 3, 'Train me in Alchemy.', 0, 5, 16);
+
+-- add missing gossip_menu_option_trainer for High Level Enchanters - the options are Apprentice or Illustrious Grand Master.
+insert into gossip_menu_option_trainer
+(MenuId, OptionIndex, TrainerId)
+values (4169, 0, 63);
+
+-- add missing gossip_menu_option_trainer for High Level Tailors- the options are Apprentice, Journeyman or Illustrious Grand Master.
+insert into gossip_menu_option_trainer
+(MenuId, OptionIndex, TrainerId)
+values (4261, 0, 163);
+
+-- add missing gossip_menu_option_trainer for Illustrious Grand Master Alchemists
+-- because the other alchemy spell lists are incomplete.
+insert into gossip_menu_option_trainer
+(MenuId, OptionIndex, TrainerId)
+values (4111, 0, 122),
+	(7384, 0, 122);
+
+-- add missing gossip_menu_option_trainer for Apprentice Blacksmiths
+insert into gossip_menu_option_trainer
+(MenuId, OptionIndex, TrainerId)
+values (2746, 0, 80),
+	(2741, 0, 80);
+
+-- add missing gossip_menu_option_trainer for Apprentice Engineers
+insert into gossip_menu_option_trainer
+(MenuId, OptionIndex, TrainerId)
+values (4142, 0, 102),
+	(7383, 0, 102);
+
+-- add missing gossip_menu_option_trainer for Apprentice Leatherworkers
+insert into gossip_menu_option_trainer
+(MenuId, OptionIndex, TrainerId)
+values (7430, 0, 103);
+
+-- add missing gossip_menu_option_trainer for Skinners
+insert into gossip_menu_option_trainer
+(MenuId, OptionIndex, TrainerId)
+values (7429, 0, 157);
+
+-- add missing gossip_menu_option_trainer for Riding Trainers
+insert into gossip_menu_option_trainer
+(MenuId, OptionIndex, TrainerId)
+values (8553, 0, 386),
+	(8275, 0, 386);
+
+-- correct cooking trainer entry
+update creature_template set gossip_menu_Id = 5853 where entry = 6286;
+-- correct leatherworking trainer entry
+update creature_template set gossip_menu_Id = 4242 where entry = 16278;
+-- correct mining trainer entry
+update creature_template set gossip_menu_Id = 7690 where entry = 3175;
+-- remove the repairer mouseover
+update creature_template set npcflag = 80 where entry = 16278;
+-- correct a mount trainer npcflag
+update creature_template set npcflag = 81 where entry = 3175;
+-- correct a mount trainer npcflag
+update creature_template set npcflag = 83 where entry = 16280;
+
+-- correct the profession trainers
+-- correct types and flags on training gossips for initial menu.
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12180 and OptionIndex between 0 and 10;
+-- add trainers to gossips for initial menu
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12180, 2, 51),
+(12180, 3, 102),
+(12180, 5, 63),
+(12180, 6, 48),
+(12180, 10, 117);
+-- change the initial alchmey trainer to one with the full spell list.
+update gossip_menu_option_trainer set TrainerId = 122 where MenuId = 12180 and OptionIndex = 0;
+
+-- correct types and flags on training gossips under "Tell me about gathering professions"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12185 and OptionIndex between 0 and 10;
+-- add trainer options for the conditional training options under "Tell me about gathering professions"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12185, 0, 122), -- alchemy
+	(12185, 1, 80), -- blacksmithing
+	(12185, 2, 51), -- enchanting
+	(12185, 3, 102), -- engineering
+	(12185, 4, 388), -- herbalism
+	(12185, 5, 63), -- inscription
+	(12185, 6, 48), -- jewelcrafting
+	(12185, 7, 103), -- leatherworking
+	(12185, 8, 389), -- mining
+	(12185, 9, 390), -- skinning
+	(12185, 10, 117); -- tailoring
+	
+-- correct types and flags on training gossips under "Tell me about Herbalism"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12188 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Herbalism"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12188, 0, 122), -- alchemy
+	(12188, 1, 80), -- blacksmithing
+	(12188, 2, 51), -- enchanting
+	(12188, 3, 102), -- engineering
+	(12188, 4, 388), -- herbalism
+	(12188, 5, 63), -- inscription
+	(12188, 6, 48), -- jewelcrafting
+	(12188, 7, 103), -- leatherworking
+	(12188, 8, 389), -- mining
+	(12188, 9, 390), -- skinning
+	(12188, 10, 117); -- tailoring
+	
+-- correct types and flags on training gossips under "Tell me about Mining"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12189 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Mining"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12189, 0, 122), -- alchemy
+	(12189, 1, 80), -- blacksmithing
+	(12189, 2, 51), -- enchanting
+	(12189, 3, 102), -- engineering
+	(12189, 4, 388), -- herbalism
+	(12189, 5, 63), -- inscription
+	(12189, 6, 48), -- jewelcrafting
+	(12189, 7, 103), -- leatherworking
+	(12189, 8, 389), -- mining
+	(12189, 9, 390), -- skinning
+	(12189, 10, 117); -- tailoring
+	
+-- correct types and flags on training gossips under "Tell me about Skinning"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12190 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Skinning"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12190, 0, 122), -- alchemy
+	(12190, 1, 80), -- blacksmithing
+	(12190, 2, 51), -- enchanting
+	(12190, 3, 102), -- engineering
+	(12190, 4, 388), -- herbalism
+	(12190, 5, 63), -- inscription
+	(12190, 6, 48), -- jewelcrafting
+	(12190, 7, 103), -- leatherworking
+	(12190, 8, 389), -- mining
+	(12190, 9, 390), -- skinning
+	(12190, 10, 117); -- tailoring
+	
+-- correct types and flags on training gossips under "Tell me about production professions"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12186 and OptionIndex between 0 and 10;
+-- add trainer options for the conditional training options under "Tell me about production professions"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12186, 0, 122), -- alchemy
+	(12186, 1, 80), -- blacksmithing
+	(12186, 2, 51), -- enchanting
+	(12186, 3, 102), -- engineering
+	(12186, 4, 388), -- herbalism
+	(12186, 5, 63), -- inscription
+	(12186, 6, 48), -- jewelcrafting
+	(12186, 7, 103), -- leatherworking
+	(12186, 8, 389), -- mining
+	(12186, 9, 390), -- skinning
+	(12186, 10, 117); -- tailoring
+
+-- correct types and flags on training gossips under "Tell me about Alchemy"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12191 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Alchemy"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12191, 0, 122), -- alchemy
+	(12191, 1, 80), -- blacksmithing
+	(12191, 2, 51), -- enchanting
+	(12191, 3, 102), -- engineering
+	(12191, 4, 388), -- herbalism
+	(12191, 5, 63), -- inscription
+	(12191, 6, 48), -- jewelcrafting
+	(12191, 7, 103), -- leatherworking
+	(12191, 8, 389), -- mining
+	(12191, 9, 390), -- skinning
+	(12191, 10, 117); -- tailoring
+-- change the alchmey trainer to one with the full spell list.
+update gossip_menu_option_trainer set TrainerId = 122 where MenuId = 12191 and OptionIndex = 11;
+
+-- correct types and flags on training gossips under "Tell me about Blacksmithing"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12192 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Blacksmithing"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12192, 1, 80), -- blacksmithing
+	(12192, 2, 51), -- enchanting
+	(12192, 3, 102), -- engineering
+	(12192, 4, 388), -- herbalism
+	(12192, 5, 63), -- inscription
+	(12192, 6, 48), -- jewelcrafting
+	(12192, 7, 103), -- leatherworking
+	(12192, 8, 389), -- mining
+	(12192, 9, 390), -- skinning
+	(12192, 10, 117); -- tailoring
+-- change the alchmey trainer to one with the full spell list.
+update gossip_menu_option_trainer set TrainerId = 122 where MenuId = 12192 and OptionIndex = 0;
+
+-- correct types and flags on training gossips under "Tell me about Enchanting"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12193 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Enchanting"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12193, 1, 80), -- blacksmithing
+	(12193, 2, 51), -- enchanting
+	(12193, 3, 102), -- engineering
+	(12193, 4, 388), -- herbalism
+	(12193, 5, 63), -- inscription
+	(12193, 6, 48), -- jewelcrafting
+	(12193, 7, 103), -- leatherworking
+	(12193, 8, 389), -- mining
+	(12193, 9, 390), -- skinning
+	(12193, 10, 117); -- tailoring
+
+-- correct types and flags on training gossips under "Tell me about Engineering"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12195 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Engineering"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12195, 1, 80), -- blacksmithing
+	(12195, 2, 51), -- enchanting
+	(12195, 3, 102), -- engineering
+	(12195, 4, 388), -- herbalism
+	(12195, 5, 63), -- inscription
+	(12195, 6, 48), -- jewelcrafting
+	(12195, 7, 103), -- leatherworking
+	(12195, 8, 389), -- mining
+	(12195, 9, 390), -- skinning
+	(12195, 10, 117); -- tailoring
+
+-- correct types and flags on training gossips under "Tell me about Inscription"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12196 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Inscription"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12196, 1, 80), -- blacksmithing
+	(12196, 2, 51), -- enchanting
+	(12196, 3, 102), -- engineering
+	(12196, 4, 388), -- herbalism
+	(12196, 5, 63), -- inscription
+	(12196, 6, 48), -- jewelcrafting
+	(12196, 7, 103), -- leatherworking
+	(12196, 8, 389), -- mining
+	(12196, 9, 390), -- skinning
+	(12196, 10, 117), -- tailoring
+	(12196, 11, 63); -- inscription 
+
+-- correct types and flags on training gossips under "Tell me about Jewelcrafting"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12197 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Jewelcrafting"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12197, 1, 80), -- blacksmithing
+	(12197, 2, 51), -- enchanting
+	(12197, 3, 102), -- engineering
+	(12197, 4, 388), -- herbalism
+	(12197, 5, 63), -- inscription
+	(12197, 6, 48), -- jewelcrafting
+	(12197, 7, 103), -- leatherworking
+	(12197, 8, 389), -- mining
+	(12197, 9, 390), -- skinning
+	(12197, 10, 117); -- tailoring
+
+-- correct types and flags on training gossips under "Tell me about Leatherworking"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12198 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Leatherworking"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12198, 1, 80), -- blacksmithing
+	(12198, 2, 51), -- enchanting
+	(12198, 3, 102), -- engineering
+	(12198, 4, 388), -- herbalism
+	(12198, 5, 63), -- inscription
+	(12198, 6, 48), -- jewelcrafting
+	(12198, 7, 103), -- leatherworking
+	(12198, 8, 389), -- mining
+	(12198, 9, 390), -- skinning
+	(12198, 10, 117); -- tailoring
+
+-- correct types and flags on training gossips under "Tell me about Tailoring"
+update gossip_menu_option set OptionType = 5, OptionNpcflag = 16 where MenuId = 12199 and OptionIndex between 0 and 11;
+-- add trainer options for the conditional training options under "Tell me about Tailoring"
+insert into gossip_menu_option_trainer (MenuId, OptionIndex, TrainerId)
+values (12199, 1, 80), -- blacksmithing
+	(12199, 2, 51), -- enchanting
+	(12199, 3, 102), -- engineering
+	(12199, 4, 388), -- herbalism
+	(12199, 5, 63), -- inscription
+	(12199, 6, 48), -- jewelcrafting
+	(12199, 7, 103), -- leatherworking
+	(12199, 8, 389), -- mining
+	(12199, 9, 390), -- skinning
+	(12199, 10, 117); -- tailoring
+	
+-- correct a few innkeepers
+update gossip_menu_option set OptionType = 8, OptionNpcflag = 65536 where MenuId = 1297 and OptionIndex = 1;
+update gossip_menu_option set OptionType = 3, OptionNpcflag = 128 where MenuId = 1297 and OptionIndex = 2;
+insert into gossip_menu_option_action (MenuId, OptionIndex, ActionMenuId, ActionPoiId) values (1297, 0, 1221, 0);
+update gossip_menu_option set OptionType = 1, OptionNpcflag = 1 where MenuId = 1293 and OptionIndex = 0;
+update gossip_menu_option set OptionType = 8, OptionNpcflag = 65536 where MenuId = 1293 and OptionIndex = 1;
+update gossip_menu_option set OptionType = 3, OptionNpcflag = 128 where MenuId = 1293 and OptionIndex = 2;
+insert into gossip_menu_option_action (MenuId, OptionIndex, ActionMenuId, ActionPoiId) values (1293, 0, 1221, 0);
+update gossip_menu_option set OptionIcon = 0, OptionText = 'What can I do at an inn?', OptionBroadcastTextId = 4308
+where MenuId = 1294 and OptionIndex = 2;
+update gossip_menu_option set OptionIcon = 0, OptionText = 'What can I do at an inn?', OptionBroadcastTextId = 4308
+where MenuId = 7468 and OptionIndex = 2;
