37f888d380f5cab0e630ba57af3845ec86052dc2
psychobilli <richardsw1123@gmail.com>
2021-01-02 23:10:48 -0500

    Updated shell scripts.

--- Scripts/Shell Scripts/wotlk_fixes.sh
+++ Scripts/Shell Scripts/wotlk_fixes.sh
@@ -4,3 +4,5 @@ echo "* * * 2020_12_13_00_GH_Vordrassil_Chain.sql"
 mysql -u jenkins -p'' world_test -s < '../TrinityCore 335 Mod/sql/updates/world/3.3.5/2020_12_13_00_GH_Vordrassil_Chain.sql'
 echo "* * * 2020_12_30_dungeon_rep.sql"
 mysql -u jenkins -p'' world_test -s < '../TrinityCore 335 Mod/sql/updates/world/3.3.5/2020_12_30_dungeon_rep.sql'
+echo "* * * 2021_01_01_00_hodir_quests.sql"
+mysql -u jenkins -p'' world_test -s < '../TrinityCore 335 Mod/sql/updates/world/3.3.5/2021_01_01_00_hodir_quests.sql'
