61cf7f35a7d5cc26a2bb0d8092fbc79259ff6c53
psychobilli <richardsw1123@gmail.com>
2021-05-13 22:01:25 -0400

    Additional updates for Rolling with my Homies.

--- Scripts/TC Cata Updates/Kezan Script Updates.sql
+++ Scripts/TC Cata Updates/Kezan Script Updates.sql
@@ -1,4 +1,4 @@
--- Rollin with my Homies
+-- Rolling with my Homies
 -- correct the Goblin Hot Rod for Rollin with my Homies (14071)
 UPDATE `creature_template` SET `npcflag` = 16777216, `vehicleId` = 448, `AIName` = 'SmartAI', `flags_extra` = 8192 WHERE `entry` = 34840;
 DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` = 34840 AND `spell_id` = 66392;
@@ -21,6 +21,7 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` IN (34892);
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (34892,0,0,1,10,0,100,512,1,5,30,30,0,33,34957,0,0,0,0,0,7,0,0,0,0,0,0,0,'Ace - On LOS KC ''Rolling with my Homies'''),
 (34892,0,1,0,61,0,100,512,0,0,0,0,0,134,66697,0,0,0,0,0,7,0,0,0,0,0,0,0,'Ace - On LOS Cast ''Rolling with my Homies: Summon Ace''');
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 34954;
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (34954);
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (34954,0,0,1,10,0,100,512,1,5,30,30,0,33,34958,0,0,0,0,0,7,0,0,0,0,0,0,0,'Gobber - On LOS KC ''Rolling with my Homies'''),
@@ -38,6 +39,10 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceGroup` 
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorType`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
 (22,2,34894,0,1,47,0,14071,8,0,0,0,0,'','Gobber trigger script while quest ''Rolling with my Homies'' in progress'),
 (22,2,34894,0,1,29,0,34958,7,0,1,0,0,'','Gobber trigger script while npc is not near');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (34874) AND `id` = 8;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(34874,0,8,0,20,0,100,512,14071,0,0,0,0,56,46856,1,0,0,0,0,7,0,0,0,0,0,0,0,'Megs Dreadshredder - On Quest Reward ''Rolling with my Homies'' Add Item');
 
 -- make Keys to the Hot Rod (28607) repeatable.
 UPDATE `quest_template_addon` SET `SpecialFlags` = 1 WHERE `Id` = 28607;
