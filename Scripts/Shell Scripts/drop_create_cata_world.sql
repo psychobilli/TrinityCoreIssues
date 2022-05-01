0e98414605896355ff8e9142f98f562472142614
psychobilli <richardsw1123@gmail.com>
2022-05-01 13:22:02 -0400

    Updated the drop_create_cata_world script to point to the correct database for resetting the world_cata database

--- Scripts/Shell Scripts/drop_create_cata_world.sql
+++ Scripts/Shell Scripts/drop_create_cata_world.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS `world_cata`;
 
-CREATE DATABASE `world_cata_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `world_cata` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON `world_cata_test`.* TO `trinity`@`localhost` WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `world_cata`.* TO `trinity`@`localhost` WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `world_cata`.* TO `jenkins`@`localhost` WITH GRANT OPTION;
