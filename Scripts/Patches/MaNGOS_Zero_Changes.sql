980d3b61d8e1c9b1c90b42caa99cd4e119ca0594
psychobilli <richardsw1123@gmail.com>
2022-12-23 20:40:19 -0500

    Added linen exchange quests to the cloth quartermasters.

--- Scripts/Patches/MaNGOS_Zero_Changes.sql
+++ Scripts/Patches/MaNGOS_Zero_Changes.sql
@@ -18,3 +18,72 @@ update zero_world.creature
 set spawntimesecs = 300
 where id = 9956
   and spawntimesecs = 600;
+  
+-- add a linen exchange to the cloth quartermasters.
+delete from zero_world.quest_template where entry in (10000,10001,10002,10003,10004,10005,10006,10007);
+insert into zero_world.quest_template
+(entry, Method, ZoneOrSort, MinLevel, QuestLevel, Type, RequiredClasses, 
+RequiredRaces, RequiredSkill, RequiredSkillValue, RepObjectiveFaction, RepObjectiveValue, RequiredMinRepFaction, RequiredMinRepValue, RequiredMaxRepFaction, RequiredMaxRepValue, SuggestedPlayers, LimitTime, QuestFlags, SpecialFlags, PrevQuestId, NextQuestId, ExclusiveGroup, NextQuestInChain, SrcItemId, SrcItemCount, SrcSpell, Title, Details, Objectives, 
+OfferRewardText, 
+RequestItemsText, 
+EndText, ObjectiveText1, ObjectiveText2, ObjectiveText3, ObjectiveText4, ReqItemId1, ReqItemId2, ReqItemId3, ReqItemId4, ReqItemCount1, ReqItemCount2, ReqItemCount3, ReqItemCount4, ReqSourceId1, ReqSourceId2, ReqSourceId3, ReqSourceId4, ReqSourceCount1, ReqSourceCount2, ReqSourceCount3, ReqSourceCount4, ReqCreatureOrGOId1, ReqCreatureOrGOId2, ReqCreatureOrGOId3, ReqCreatureOrGOId4, ReqCreatureOrGOCount1, ReqCreatureOrGOCount2, ReqCreatureOrGOCount3, ReqCreatureOrGOCount4, ReqSpellCast1, ReqSpellCast2, ReqSpellCast3, ReqSpellCast4, RewChoiceItemId1, RewChoiceItemId2, RewChoiceItemId3, RewChoiceItemId4, RewChoiceItemId5, RewChoiceItemId6, RewChoiceItemCount1, RewChoiceItemCount2, RewChoiceItemCount3, RewChoiceItemCount4, RewChoiceItemCount5, RewChoiceItemCount6, RewItemId1, RewItemId2, RewItemId3, RewItemId4, RewItemCount1, RewItemCount2, RewItemCount3, RewItemCount4, RewRepFaction1, RewRepFaction2, RewRepFaction3, RewRepFaction4, RewRepFaction5, RewRepValue1, RewRepValue2, RewRepValue3, RewRepValue4, RewRepValue5, RewOrReqMoney, RewMoneyMaxLevel, RewSpell, RewSpellCast, RewMailTemplateId, RewMailDelaySecs, PointMapId, PointX, PointY, PointOpt, DetailsEmote1, DetailsEmote2, DetailsEmote3, DetailsEmote4, DetailsEmoteDelay1, DetailsEmoteDelay2, DetailsEmoteDelay3, DetailsEmoteDelay4, IncompleteEmote, CompleteEmote, OfferRewardEmote1, OfferRewardEmote2, OfferRewardEmote3, OfferRewardEmote4, OfferRewardEmoteDelay1, OfferRewardEmoteDelay2, OfferRewardEmoteDelay3, OfferRewardEmoteDelay4, StartScript, CompleteScript)
+values ('10000', '0', '1519', '20', '60', '0', '0', 
+'77', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Linen Exchange', '', '', 
+'$N - we can exchange linen for wool. Thank you for your continued efforts!', 
+'Whenever you have enough linen, I\'ll accept it.', 
+'', '', '', '', '', '2589', '0', '0', '0', '40', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2592', '0', '0', '0', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+, ('10001', '0', '1657', '20', '60', '0', '0', 
+'77', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Linen Exchange', '', '', 
+'$N - we can exchange linen for wool. Thank you for your continued efforts!', 
+'Whenever you have enough linen, I\'ll accept it.', 
+'', '', '', '', '', '2589', '0', '0', '0', '40', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2592', '0', '0', '0', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+, ('10002', '0', '1537', '20', '60', '0', '0', 
+'77', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Linen Exchange', '', '', 
+'$N - we can exchange linen for wool. Thank you for your continued efforts!', 
+'Whenever you have enough linen, I\'ll accept it.', 
+'', '', '', '', '', '2589', '0', '0', '0', '40', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2592', '0', '0', '0', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+, ('10003', '0', '1537', '20', '60', '0', '0', 
+'77', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Linen Exchange', '', '', 
+'$N - we can exchange linen for wool. Thank you for your continued efforts!', 
+'Whenever you have enough linen, I\'ll accept it.', 
+'', '', '', '', '', '2589', '0', '0', '0', '40', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2592', '0', '0', '0', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+, ('10004', '0', '1497', '20', '60', '0', '0', 
+'178', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Linen Exchange', '', '', 
+'$N - we can exchange linen for wool. Thank you for your continued efforts!', 
+'Whenever you have enough linen, I\'ll accept it.', 
+'', '', '', '', '', '2589', '0', '0', '0', '40', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2592', '0', '0', '0', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+, ('10005', '0', '1638', '20', '60', '0', '0', 
+'178', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Linen Exchange', '', '', 
+'$N - we can exchange linen for wool. Thank you for your continued efforts!', 
+'Whenever you have enough linen, I\'ll accept it.', 
+'', '', '', '', '', '2589', '0', '0', '0', '40', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2592', '0', '0', '0', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+, ('10006', '0', '1637', '20', '60', '0', '0', 
+'178', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Linen Exchange', '', '', 
+'$N - we can exchange linen for wool. Thank you for your continued efforts!', 
+'Whenever you have enough linen, I\'ll accept it.', 
+'', '', '', '', '', '2589', '0', '0', '0', '40', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2592', '0', '0', '0', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+, ('10007', '0', '1637', '20', '60', '0', '0', 
+'178', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Linen Exchange', '', '', 
+'$N - we can exchange linen for wool. Thank you for your continued efforts!', 
+'Whenever you have enough linen, I\'ll accept it.', 
+'', '', '', '', '', '2589', '0', '0', '0', '40', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2592', '0', '0', '0', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+
+delete from zero_world.quest_relations where quest in (10000,10001,10002,10003,10004,10005,10006,10007);
+insert into zero_world.quest_relations
+(actor, entry, quest, role)
+values ('0', '14722', '10000', '0'),
+	('0', '14722', '10000', '1'),
+    ('0', '14723', '10001', '0'),
+	('0', '14723', '10001', '1'),
+    ('0', '14724', '10002', '0'),
+	('0', '14724', '10002', '1'),
+    ('0', '14725', '10003', '0'),
+	('0', '14725', '10003', '1'),
+    ('0', '14726', '10004', '0'),
+	('0', '14726', '10004', '1'),
+    ('0', '14727', '10005', '0'),
+	('0', '14727', '10005', '1'),
+    ('0', '14728', '10006', '0'),
+	('0', '14728', '10006', '1'),
+    ('0', '14729', '10007', '0'),
+	('0', '14729', '10007', '1');
